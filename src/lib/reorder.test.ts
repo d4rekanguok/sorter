12dcb4b0594d009c9aba10217b63883b5cf5e1c2
Derek Nguyen <derek@penandpillow.com>
2021-04-13 00:06:22 +0900

    feat: handle a few edge cases

--- src/lib/reorder.test.ts
+++ src/lib/reorder.test.ts
@@ -25,4 +25,12 @@ test('reorder multiple items with backward orders', () => {
   assert.equal(reorder(array, from, to), ['A', 'C', 'E', 'G', 'D', 'B', 'F'] )
 })
 
+test('reorder faulty items', () => {
+  const array = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+  const from = [3, 100]
+  const to = 4
+  // ['A', 'B', 'C', 'E', 'F', 'G']
+  assert.equal(reorder(array, from, to), ['A', 'B', 'C', 'E', 'D', 'F', 'G'] )
+})
+
 test.run()
--- src/lib/reorder.ts
+++ src/lib/reorder.ts
@@ -5,15 +5,16 @@
  * @param to target index without offset
  */
 export const reorder = <T>(array: T[], from: number[], to: number) => {
+  const filteredFrom = from.filter(v => v < array.length && v >= 0)
   // sort id so it's easier to offset index
-  const orderedFrom = from.slice().sort()
+  const orderedFrom = filteredFrom.slice().sort()
   // map id to item
   const movedItems = orderedFrom.reduce((acc, cur, i) => {
     acc[cur] = array.splice(cur - i, 1)[0]
     return acc
   }, {})
   // put items back to the correct order
-  const orderedItems = from.map(id => movedItems[id])
+  const orderedItems = filteredFrom.map(id => movedItems[id])
   array.splice(to, 0, ...orderedItems);
   return array
 }
